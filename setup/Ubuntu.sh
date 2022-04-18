3c961e75f3069f4fc3163fa26ab19bc113a78d52
Yukun <ykshang4119@gmail.com>
2022-04-18 15:07:10 +0800

    sync

--- setup/Ubuntu.sh
+++ setup/Ubuntu.sh
@@ -1,6 +1,6 @@
 sudo apt update && sudo apt upgrade -y
 # change to zsh
-sudo apt install zsh git curl wget
+sudo apt install zsh git curl wget htop
 chsh -s /bin/zsh
 # Install Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
