bda9fcfb374375a9a317f1f418f7a0093fb3db44
Yukun <ykshang4119@gmail.com>
2022-04-18 15:15:08 +0800

    sync

--- setup/Ubuntu.sh
+++ setup/Ubuntu.sh
@@ -1,6 +1,6 @@
 sudo apt update && sudo apt upgrade -y
 # change to zsh
-sudo apt install zsh git curl wget htop
+sudo apt install zsh git curl wget htop -y
 chsh -s /bin/zsh
 # Install Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -12,10 +12,10 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 echo "source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 
 # autojump
-sudo apt install autojump
+sudo apt install autojump -y
 
 # neovim
 sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt-get update
-sudo apt-get install neovim
+sudo apt-get update -y
+sudo apt-get install neovim -y
 
