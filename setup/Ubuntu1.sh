fc0da114b438b5c002355ac68f05433950cec294
Yukun Shang <ykshang4119@gmail.com>
2022-11-18 12:42:09 +0800

    update

--- setup/Ubuntu1.sh
+++ setup/Ubuntu1.sh
@@ -53,7 +53,7 @@ curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo 
 echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
 sudo wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_20.04/Release.key -O "/etc/apt/trusted.gpg.d/home:manuelschneid3r.asc"
 sudo apt update
-sudo apt install albert
+sudo apt install albert -y
 
 
 # echo "Installing htop with snap"
