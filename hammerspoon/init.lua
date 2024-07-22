7f64b3d84069e646fdca2f67d9a353bc8c40e9bb
Ryan Shang <50010920+ryqdev@users.noreply.github.com>
2024-07-22 12:44:04 +0800

    chore: Update init.lua

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,4 +1,3 @@
--- Window managemente
 hs.hotkey.bind({"cmd", "alt"}, "H", function()
     local win = hs.window.focusedWindow()
     local f = win:frame()
@@ -63,3 +62,47 @@ hs.hotkey.bind({"cmd", "alt"}, "F", function()
     f.h = max.h
     win:setFrame(f)
   end)
+
+
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Config loaded")
+
+
+menubarFont = hs.menubar.new()
+menubarFont:setTitle("ryqdev")
+
+function menubarClicked()
+  setCaffeineDisplay(hs.alert.show("Who is your daddy?"))
+end
+
+if menubarFont then
+  menubarFont:setClickCallback(menubarClicked)
+end
+
+
+
+switcher = hs.window.switcher.new(hs.window.filter.new():setCurrentSpace(true))
+function mapCmdTab(event)
+    local flags = event:getFlags()
+    local chars = event:getCharacters()
+    if chars == "\t" and flags:containExactly{'cmd'} then
+        switcher:next()
+        return true
+    elseif chars == string.char(25) and flags:containExactly{'cmd','shift'} then
+        switcher:previous()
+        return true
+    end
+end
+tapCmdTab = hs.eventtap.new({hs.eventtap.event.types.keyDown}, mapCmdTab)
+tapCmdTab:start()
