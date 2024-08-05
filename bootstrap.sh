ba608c542c7e824e1d9a3a94d79d3096e2856e69
ryqdev <ykshang4119@gmail.com>
2024-08-05 23:27:32 +0800

    chore: add miniconda

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,12 @@
 # install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
+# anaconda 
+#Downloading the latest Miniconda installer for Linux. Your architecture may vary.
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+bash ~/miniconda.sh -b -p $HOME/miniconda
+eval "$(/$HOME/miniconda/bin/conda shell.zsh hook)"
+
 # setup shell
 echo "changing shell to zsh"
 sudo apt install zsh git curl wget htop -y
