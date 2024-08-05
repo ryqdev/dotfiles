a25fccf19efd733f7460e495b6bdefac7b2dc04c
ryqdev <ykshang4119@gmail.com>
2024-08-05 23:24:02 +0800

    chore: add in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,17 @@
 # install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# setup shell
+echo "changing shell to zsh"
+sudo apt install zsh git curl wget htop -y
+
+chsh -s $(which zsh)
+# Install Oh-My-Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# auto suggestion
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
