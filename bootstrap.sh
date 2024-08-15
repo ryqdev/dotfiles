9f320985f7acf22150d82d938bc65848df2cabb4
Ryan Shang <50010920+ryqdev@users.noreply.github.com>
2024-08-15 17:34:33 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,4 +27,11 @@ echo "Updating .zshrc..."
     echo "source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 } >> ${ZDOTDIR:-$HOME}/.zshrc
 
+echo "Installing lazygit"
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
+
 echo "Setup complete!"
