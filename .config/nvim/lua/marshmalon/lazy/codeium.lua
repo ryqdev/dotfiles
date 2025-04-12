2997fa7bcdfc35df47b0d60921c5969955052130
Ryan Shang <50010920+ryqdev@users.noreply.github.com>
2025-04-12 12:13:45 +0800

    chore: fix possible typo

--- .config/nvim/lua/marshmalon/lazy/codeium.lua
+++ .config/nvim/lua/marshmalon/lazy/codeium.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     vim.g.codeium_log_file = "~/.codeium/codeium.log"
     vim.g.codeium_no_map_tab = 1
-    vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+    vim.keymap.set('i', '<c-g>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
     vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end,
       { expr = true, silent = true })
     vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
