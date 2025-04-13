92a3589100b7503378453c3af4e526403f6d2ccf
Ryan Shang <50010920+ryqdev@users.noreply.github.com>
2025-04-13 11:51:34 +0800

    feat: enable h and l to unfold/fold in nvimtree

--- .config/nvim/lua/marshmalon/lazy/nvimtree.lua
+++ .config/nvim/lua/marshmalon/lazy/nvimtree.lua
@@ -6,8 +6,27 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
+    local function my_on_attach(bufnr)
+      local api = require "nvim-tree.api"
+
+      local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+
+      -- default mappings
+      api.config.mappings.default_on_attach(bufnr)
+
+      -- custom mappings
+      vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts('Up'))
+      vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+      vim.keymap.set('n', 'l', api.node.open.edit, { buffer = bufnr, desc = "Open" })
+      vim.keymap.set('n', 'h', api.node.navigate.parent_close, { buffer = bufnr, desc = "Close Directory" })
+    end
+
     require("nvim-tree").setup(
       {
+        on_attach = my_on_attach,
+
         filters = {
           dotfiles = false,
           git_ignored = false
