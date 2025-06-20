53bf37433e8a3d52a18281f8ef7e62d9be22a962
Ryan Shang <50010920+ryqdev@users.noreply.github.com>
2025-06-20 23:31:53 +0800

    chore: update neovim

--- .config/nvim/lua/marshmalon/remap.lua
+++ .config/nvim/lua/marshmalon/remap.lua
@@ -7,19 +7,17 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local opts = { buffer = event.buf }
 
     local mappings = {
-      { "gd", vim.lsp.buf.definition, desc = "Go to definition", buffer = event.buf },
-      { "gl", vim.diagnostic.open_float, desc = "Open diagnostic float", buffer = event.buf },
-      { "gr", vim.lsp.buf.references, desc = "Go to references", buffer = event.buf },
-      { "gi", vim.lsp.buf.implementation, desc = "Go to implementation", buffer = event.buf },
-      { "K", vim.lsp.buf.hover, desc = "Show hover information", buffer = event.buf },
-      { "<leader>l", group = "LSP", buffer = event.buf },
-      { "<leader>la", vim.lsp.buf.code_action, desc = "Code action", buffer = event.buf },
-      { "<leader>lr", vim.lsp.buf.references, desc = "References", buffer = event.buf },
-      { "<leader>ln", vim.lsp.buf.rename, desc = "Rename", buffer = event.buf },
-      { "<leader>lw", vim.lsp.buf.workspace_symbol, desc = "Workspace symbol", buffer = event.buf },
-      { "<leader>ld", vim.diagnostic.open_float, desc = "Open diagnostic float", buffer = event.buf },
-      { "[d", vim.diagnostic.goto_next, desc = "Go to next diagnostic", buffer = event.buf },
-      { "]d", vim.diagnostic.goto_prev, desc = "Go to previous diagnostic", buffer = event.buf },
+      { "gd",         vim.lsp.buf.definition,       desc = "Go to definition",       buffer = event.buf },
+      { "gl",         vim.diagnostic.open_float,    desc = "Open diagnostic float",  buffer = event.buf },
+      { "gr",         vim.lsp.buf.references,       desc = "Go to references",       buffer = event.buf },
+      { "gi",         vim.lsp.buf.implementation,   desc = "Go to implementation",   buffer = event.buf },
+      { "K",          vim.lsp.buf.hover,            desc = "Show hover information", buffer = event.buf },
+      { "<leader>l",  group = "LSP",                buffer = event.buf },
+      { "<leader>la", vim.lsp.buf.code_action,      desc = "Code action",            buffer = event.buf },
+      { "<leader>lr", vim.lsp.buf.references,       desc = "References",             buffer = event.buf },
+      { "<leader>ln", vim.lsp.buf.rename,           desc = "Rename",                 buffer = event.buf },
+      { "<leader>lw", vim.lsp.buf.workspace_symbol, desc = "Workspace symbol",       buffer = event.buf },
+      { "<leader>ld", vim.diagnostic.open_float,    desc = "Open diagnostic float",  buffer = event.buf },
     }
 
     which_key.add(mappings)
@@ -35,24 +33,23 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 local non_lsp_mappings = {
-  { "<leader> ", ":Telescope live_grep<CR>", desc = "Find" },
-  { "<leader>e", vim.cmd.Ex, desc = "Open file explorer" },
-  { "<leader>p", "\"_dP", desc = "Paste without overwrite" },
-  { "<leader>/", "<Plug>(comment_toggle_linewise_current)", desc = "Toggle comment" },
-  { "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], desc = "Search and replace word under cursor" },
-  { "<leader>t", ":Today<CR>", desc = "Open today's note" },
-  { "<leader>f", group = "Find" },
-  { "<leader>ff", builtin.find_files, desc = "Find files" },
-  { "<leader>fg", builtin.git_files, desc = "Find git files" },
-  { "<leader>fl", builtin.live_grep, desc = "Live grep" },
-  { "<leader>y", group = "Copy" },
-  { "<leader>yp", ':let @+ = expand("%:p")<CR>', desc = "Copy full path" },
-  { "<C-d>", "<C-d>zz", desc = "Half page down and center" },
-  { "<C-u>", "<C-u>zz", desc = "Half page up and center" },
-  { "n", "nzzzv", desc = "Next search result and center" },
-  { "N", "Nzzzv", desc = "Previous search result and center" },
-  { "Q", "<nop>", desc = "Disable Ex mode" },
-  { ";", builtin.buffers, desc = "Find buffers" },
+  { "<leader> ",  ":Telescope live_grep<CR>",                             desc = "Find" },
+  { "<leader>p",  "\"_dP",                                                desc = "Paste without overwrite" },
+  { "<leader>/",  "<Plug>(comment_toggle_linewise_current)",              desc = "Toggle comment" },
+  { "<leader>s",  [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], desc = "Search and replace word under cursor" },
+  { "<leader>ot", ":terminal<CR>",                                        desc = "Open terminal" },
+  { "<leader>f",  group = "Find" },
+  { "<leader>ff", builtin.find_files,                                     desc = "Find files" },
+  { "<leader>fg", builtin.git_files,                                      desc = "Find git files" },
+  { "<leader>fl", builtin.live_grep,                                      desc = "Live grep" },
+  { "<leader>y",  group = "Copy" },
+  { "<leader>yp", ':let @+ = expand("%:p")<CR>',                          desc = "Copy full path" },
+  { "<C-d>",      "<C-d>zz",                                              desc = "Half page down and center" },
+  { "<C-u>",      "<C-u>zz",                                              desc = "Half page up and center" },
+  { "n",          "nzzzv",                                                desc = "Next search result and center" },
+  { "N",          "Nzzzv",                                                desc = "Previous search result and center" },
+  { "Q",          "<nop>",                                                desc = "Disable Ex mode" },
+  { ";",          builtin.buffers,                                        desc = "Find buffers" },
 }
 
 which_key.add(non_lsp_mappings)
