b4f09472679b3ed62fccada026bab47b45dfce5d
Ryan Shang <ykshang4119@gmail.com>
2025-07-16 20:00:58 +0800

    chore: update theme of neovim

--- .config/nvim/lua/marshmalon/lazy/colorscheme.lua
+++ .config/nvim/lua/marshmalon/lazy/colorscheme.lua
@@ -1,13 +1,13 @@
 return {
-  -- add dracula
-  { "Mofiqul/dracula.nvim" },
-
-  -- Configure LazyVim to load dracula
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "dracula",
-    },
-  },
+  'maxmx03/solarized.nvim',
+  lazy = false,
+  priority = 1000,
+  ---@type solarized.config
+  opts = {},
+  config = function(_, opts)
+    vim.o.termguicolors = true
+    vim.o.background = 'light'
+    require('solarized').setup(opts)
+    vim.cmd.colorscheme 'solarized'
+  end,
 }
-
--- .config/nvim/lua/marshmalon/set.lua
+++ .config/nvim/lua/marshmalon/set.lua
@@ -1,4 +1,4 @@
-vim.cmd.colorscheme("dracula")
+vim.cmd.colorscheme("solarized")
 
 vim.opt.clipboard = 'unnamedplus' -- use system keyboard for yank
 
