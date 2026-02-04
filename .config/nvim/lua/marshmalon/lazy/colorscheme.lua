bdb9d1b2e2a4f0d5294b9ebd1b79029e5472e560
ryqdev <ykshang4119@gmail.com>
2026-02-04 14:18:47 +0800

    chore: update theme for neovim

--- .config/nvim/lua/marshmalon/lazy/colorscheme.lua
+++ .config/nvim/lua/marshmalon/lazy/colorscheme.lua
@@ -2,10 +2,21 @@ return {
   "navarasu/onedark.nvim",
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
+    local style = 'dark'
+    -- Detect macOS system appearance
+    local handle = io.popen('defaults read -g AppleInterfaceStyle 2>/dev/null')
+    if handle then
+      local result = handle:read('*a')
+      handle:close()
+      if not result:match('Dark') then
+        style = 'light'
+      end
+    end
+
     require('onedark').setup {
-      style = 'dark'
+      style = style,
+      transparent = true,
     }
-    -- Enable theme
     require('onedark').load()
   end
 }
