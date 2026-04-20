34cd989eed0b73e51c1ded7b67a516dc29af52db
ryqdev <ykshang4119@gmail.com>
2026-04-20 15:02:36 +0800

    chore: keep the size of left pane

--- .config/nvim/lua/marshmalon/lazy/nvimtree.lua
+++ .config/nvim/lua/marshmalon/lazy/nvimtree.lua
@@ -27,11 +27,38 @@ return {
       {
         on_attach = my_on_attach,
 
+        hijack_directories = {
+          enable = false,
+          auto_open = false,
+        },
+
+        view = {
+          width = 30,
+        },
+
+        actions = {
+          open_file = {
+            resize_window = false,
+          },
+        },
+
         filters = {
           dotfiles = false,
           git_ignored = false
         }
       }
     )
+
+    vim.api.nvim_create_autocmd("VimEnter", {
+      callback = function(data)
+        if vim.fn.isdirectory(data.file) ~= 1 then
+          return
+        end
+        vim.cmd.enew()
+        vim.cmd.bw(data.buf)
+        vim.cmd.cd(data.file)
+        require("nvim-tree.api").tree.open({ current_window = false })
+      end,
+    })
   end
 }
